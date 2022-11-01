8d2d76377fda3ced06aa57746f0aefb74b6c317a
jwor12427 <jwor12427@gmail.com>
2022-11-01 15:49:03 +0900

    2022.11.01

--- src/components/section/Slider.js
+++ src/components/section/Slider.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper";
@@ -5,75 +7,87 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
+const sliderInfo = {
+	sub: "birds",
+	title: "For the Birds",
+	desc: "지금부터 당신에게 들려드릴 조금은 특별한 <br /> 새들에 대한 이야기 입니다.",
+	link: "/",
+	more: "자세히 보기",
+	site: "사이트 보기",
+	className: "black",
+};
+
 function Slider(props) {
-  return (
-    <>
-    <div id="sliderType" className={`slider__wrap ${props.ponts}`}>
-      <div className="slider__inner">
-          <Swiper
-            autoplay={{
-              delay: 3000,
-              disableOnInteraction: false,
-            }}
-            navigation={true}
-            pagination={{ clickable: true }}
-            modules={[Navigation, Pagination, Autoplay]}
-            className="mySwiper"
-          >
-            <SwiperSlide>
-              <div className="desc">
-                <span>birds</span>
-                <h3>For the Birds</h3>
-                <p>
-                  지금부터 당신에게 들려드릴 조금은 특별한 <br />
-                  새들에 대한 이야기 입니다.
-                </p>
-                <div className="btn">
-                  <a href="/">자세히 보기</a>
-                  <a href="/" className="black">
-                    사이트 보기
-                  </a>
-                </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="desc">
-                <span>birds</span>
-                <h3>For the Birds</h3>
-                <p>
-                  지금부터 당신에게 들려드릴 조금은 특별한 <br />
-                  새들에 대한 이야기 입니다.
-                </p>
-                <div className="btn">
-                  <a href="/">자세히 보기</a>
-                  <a href="/" className="black">
-                    사이트 보기
-                  </a>
-                </div>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="desc">
-                <span>birds</span>
-                <h3>For the Birds</h3>
-                <p>
-                  지금부터 당신에게 들려드릴 조금은 특별한 <br />
-                  새들에 대한 이야기 입니다.
-                </p>
-                <div className="btn">
-                  <a href="/">자세히 보기</a>
-                  <a href="/" className="black">
-                    사이트 보기
-                  </a>
-                </div>
-              </div>
-            </SwiperSlide>
-          </Swiper>
-        </div>
-      </div> 
-      
-    </>
-  );
+	return (
+		<>
+			<div
+				id="sliderType"
+				className={`slider__wrap ${props.ponts}`}
+			>
+				<div className="slider__inner">
+					<Swiper
+						autoplay={{
+							delay: 3000,
+							disableOnInteraction: false,
+						}}
+						navigation={true}
+						pagination={{ clickable: true }}
+						modules={[Navigation, Pagination, Autoplay]}
+						className="mySwiper"
+					>
+						<SwiperSlide>
+							<div className="desc">
+								<span>{sliderInfo.sub}</span>
+								<h3>F{sliderInfo.title}</h3>
+								<p>{sliderInfo.desc}</p>
+								<div className="btn">
+									<a href={sliderInfo.link}>{sliderInfo.more}</a>
+									<a
+										href={sliderInfo.link}
+										className={`${sliderInfo.className}`}
+									>
+										{sliderInfo.site}
+									</a>
+								</div>
+							</div>
+						</SwiperSlide>
+						<SwiperSlide>
+							<div className="desc">
+								<span>{sliderInfo.sub}</span>
+								<h3>F{sliderInfo.title}</h3>
+								<p>{sliderInfo.desc}</p>
+								<div className="btn">
+									<a href={sliderInfo.link}>{sliderInfo.more}</a>
+									<a
+										href={sliderInfo.link}
+										className={`${sliderInfo.className}`}
+									>
+										{sliderInfo.site}
+									</a>
+								</div>
+							</div>
+						</SwiperSlide>
+						<SwiperSlide>
+							<div className="desc">
+								<span>{sliderInfo.sub}</span>
+								<h3>F{sliderInfo.title}</h3>
+								<p>{sliderInfo.desc}</p>
+								<div className="btn">
+									<a href={sliderInfo.link}>{sliderInfo.more}</a>
+									<a
+										href={sliderInfo.link}
+										className={`${sliderInfo.className}`}
+									>
+										{sliderInfo.site}
+									</a>
+								</div>
+							</div>
+						</SwiperSlide>
+					</Swiper>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Slider;
--- src/components/section/Text.js
+++ src/components/section/Text.js
@@ -1,77 +1,173 @@
+/** @format */
+
 import React from "react";
+const textTitle = {
+	subTitle: "새롭게 업데이트 된 앵무새에 종류",
+	title: "멋쟁이, 따라쟁이 앵무새🦜",
+};
+
+const textInfo = [
+	{
+		infoTitle: "금강앵무",
+		infoDesc:
+			"금강앵무는 앵무새는 대부분의 매체에서 앵무새 하면 나오는 앵무새입니다. 깃털색이 화려하며, 크기가 대형종이 속하는 앵무새 입니다.",
+		infoName: "t1",
+		infoLink: "/",
+	},
+	{
+		infoTitle: "회색앵무",
+		infoDesc:
+			"회색앵무는 앵무새중 가장 지능이 높다고 알려진 새입니다. 세계에서 가장 유명한 회색앵무는 이렌느 페퍼버그 박사의 알렉스입니다.",
+		infoName: "t2",
+		infoLink: "/",
+	},
+	{
+		infoTitle: "왕관앵무",
+		infoDesc:
+			"회색앵무는 앵무새중 가장 지능이 높다고 알려진 새입니다. 세계에서 가장 유명한 회색앵무는 이렌느 페퍼버그 박사의 알렉스입니다.",
+		infoName: "t3",
+		infoLink: "/",
+	},
+	{
+		infoTitle: "코뉴어",
+		infoDesc:
+			"가장 대중적인 앵무새이며, 애교가 많고 사람을 잘 따르는 앵무새 중 손꼽힙니다. 주로 좁은 곳이나 틈새를 파고 드는 습성이 있습니다.",
+		infoName: "t4",
+		infoLink: "/",
+	},
+	{
+		infoTitle: "장미앵무",
+		infoDesc:
+			"한국에서 많이 키우는 종류 중 하나이며, 화려한 색채 때문에 애완용으로 인기가 많습니다. 하지만 겁이 많고 예민한 성격에 자주 데리고 놀면 스트레스를 받습니다.",
+		infoName: "t5",
+		infoLink: "/",
+	},
+	{
+		infoTitle: "모란앵무",
+		infoDesc:
+			"모란앵무는 성격이 매우 활달하고 호기심이 많아 부리를 써서 탐색하는 것을 좋아합니다. 주인과 유대감이 매우 깊을 때는 주인의 옆에 꼭 붙어 있는 것이 특징입니다.",
+		infoName: "t6",
+		infoLink: "/",
+	},
+];
+
+const TextMain = ({ infoTitle, infoDesc, infoName, infoLink }) => {
+	return (
+		<div className={`text ${infoName}`}>
+			<h3 className="text__title">{infoTitle}</h3>
+			<p className="text__desc">{infoDesc}</p>
+			<a
+				className="text_btn"
+				href={infoLink}
+			>
+				더보기
+			</a>
+		</div>
+	);
+};
 
 function Text({ attr }) {
-  return (
-    <section id="textType" className={`text__wrap ${attr[0]} ${attr[1]}`}>
-      <span>새롭게 업데이트 된 앵무새에 종류</span>
-      <h2 className="mb70">멋쟁이, 따라쟁이 앵무새🦜</h2>
-      <div className={`text__inner ${attr[2]}`}>
-        <div className="text t1">
-          <h3 className="text__title">금강앵무</h3>
-          <p className="text__desc">
-            금강앵무는 앵무새는 대부분의 매체에서 앵무새 하면 나오는
-            앵무새입니다. 깃털색이 화려하며, 크기가 대형종이 속하는 앵무새
-            입니다.
-          </p>
-          <a className="text_btn" href="/">
-            더보기
-          </a>
-        </div>
-        <div className="text t2">
-          <h3 className="text__title">회색앵무</h3>
-          <p className="text__desc">
-            회색앵무는 앵무새중 가장 지능이 높다고 알려진 새입니다. 세계에서
-            가장 유명한 회색앵무는 이렌느 페퍼버그 박사의 알렉스입니다.
-          </p>
-          <a className="text_btn" href="/">
-            더보기
-          </a>
-        </div>
-        <div className="text t3">
-          <h3 className="text__title">왕관앵무</h3>
-          <p className="text__desc">
-            온순한 성격으로 길들이기 쉬워서 많은 사람들이 앵무새를 처음 접할 때
-            많이 추천하는 종 중 하나입니다. 겁이 많고 소심한 것이 특징입니다.
-          </p>
-          <a className="text_btn" href="/">
-            더보기
-          </a>
-        </div>
-        <div className="text t4">
-          <h3 className="text__title">코뉴어</h3>
-          <p className="text__desc">
-            가장 대중적인 앵무새이며, 애교가 많고 사람을 잘 따르는 앵무새 중
-            손꼽힙니다. 주로 좁은 곳이나 틈새를 파고 드는 습성이 있습니다.
-          </p>
-          <a className="text_btn" href="/">
-            더보기
-          </a>
-        </div>
-        <div className="text t5">
-          <h3 className="text__title">장미앵무</h3>
-          <p className="text__desc">
-            한국에서 많이 키우는 종류 중 하나이며, 화려한 색채 때문에 애완용으로
-            인기가 많습니다. 하지만 겁이 많고 예민한 성격에 자주 데리고 놀면
-            스트레스를 받습니다.
-          </p>
-          <a className="text_btn" href="/">
-            더보기
-          </a>
-        </div>
-        <div className="text t6">
-          <h3 className="text__title">모란앵무</h3>
-          <p className="text__desc">
-            모란앵무는 성격이 매우 활달하고 호기심이 많아 부리를 써서 탐색하는
-            것을 좋아합니다. 주인과 유대감이 매우 깊을 때는 주인의 옆에 꼭 붙어
-            있는 것이 특징입니다.
-          </p>
-          <a className="text_btn" href="/">
-            더보기
-          </a>
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section
+			id="textType"
+			className={`text__wrap ${attr[0]} ${attr[1]}`}
+		>
+			<span>{textTitle.subTitle}</span>
+			<h2 className="mb70">{textTitle.title}</h2>
+			<div className={`text__inner ${attr[2]}`}>
+				{textInfo.map((info, index) => (
+					<TextMain
+						key={index}
+						infoTitle={info.infoTitle}
+						infoDesc={info.infoDesc}
+						infoName={info.infoName}
+						infoLink={info.infoLink}
+					/>
+				))}
+				{/* <div className="text t1">
+					<h3 className="text__title">금강앵무</h3>
+					<p className="text__desc">
+						금강앵무는 앵무새는 대부분의 매체에서 앵무새 하면 나오는
+						앵무새입니다. 깃털색이 화려하며, 크기가 대형종이 속하는 앵무새
+						입니다.
+					</p>
+					<a
+						className="text_btn"
+						href="/"
+					>
+						더보기
+					</a>
+				</div>
+				<div className="text t2">
+					<h3 className="text__title">회색앵무</h3>
+					<p className="text__desc">
+						회색앵무는 앵무새중 가장 지능이 높다고 알려진 새입니다. 세계에서
+						가장 유명한 회색앵무는 이렌느 페퍼버그 박사의 알렉스입니다.
+					</p>
+					<a
+						className="text_btn"
+						href="/"
+					>
+						더보기
+					</a>
+				</div>
+				<div className="text t3">
+					<h3 className="text__title">왕관앵무</h3>
+					<p className="text__desc">
+						온순한 성격으로 길들이기 쉬워서 많은 사람들이 앵무새를 처음 접할 때
+						많이 추천하는 종 중 하나입니다. 겁이 많고 소심한 것이 특징입니다.
+					</p>
+					<a
+						className="text_btn"
+						href="/"
+					>
+						더보기
+					</a>
+				</div>
+				<div className="text t4">
+					<h3 className="text__title">코뉴어</h3>
+					<p className="text__desc">
+						가장 대중적인 앵무새이며, 애교가 많고 사람을 잘 따르는 앵무새 중
+						손꼽힙니다. 주로 좁은 곳이나 틈새를 파고 드는 습성이 있습니다.
+					</p>
+					<a
+						className="text_btn"
+						href="/"
+					>
+						더보기
+					</a>
+				</div>
+				<div className="text t5">
+					<h3 className="text__title">장미앵무</h3>
+					<p className="text__desc">
+						한국에서 많이 키우는 종류 중 하나이며, 화려한 색채 때문에 애완용으로
+						인기가 많습니다. 하지만 겁이 많고 예민한 성격에 자주 데리고 놀면
+						스트레스를 받습니다.
+					</p>
+					<a
+						className="text_btn"
+						href="/"
+					>
+						더보기
+					</a>
+				</div>
+				<div className="text t6">
+					<h3 className="text__title">모란앵무</h3>
+					<p className="text__desc">
+						모란앵무는 성격이 매우 활달하고 호기심이 많아 부리를 써서 탐색하는
+						것을 좋아합니다. 주인과 유대감이 매우 깊을 때는 주인의 옆에 꼭 붙어
+						있는 것이 특징입니다.
+					</p>
+					<a
+						className="text_btn"
+						href="/"
+					>
+						더보기
+					</a>
+				</div> */}
+			</div>
+		</section>
+	);
 }
 
 export default Text;
